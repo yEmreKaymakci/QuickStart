63f87da817bc35e8b392e2ef4d29564d8f5eca56
yEmreKaymakci <yunusemrekymkci@gmail.com>
2026-02-18 22:02:21 +0300

    commit

---
+++ QuickStart.WebUI/Controllers/TestimonialController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QuickStart.WebUI.Dtos.Testimonials;
+
+namespace QuickStart.WebUI.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://localhost:7051/api/Testimonial");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
