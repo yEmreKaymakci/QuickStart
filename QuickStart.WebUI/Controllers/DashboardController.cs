3eda18aa890b5a979dce2f8c36e5e16e0ad5d50c
yEmreKaymakci <yunusemrekymkci@gmail.com>
2026-02-28 01:48:03 +0300

    commit

--- QuickStart.WebUI/Controllers/DashboardController.cs
+++ QuickStart.WebUI/Controllers/DashboardController.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace QuickStart.WebUI.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DashboardController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7051/api/Testimonial/TestimonialCount");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            ViewBag.TestimonialCount = jsonData;
+
+            var responseMessage1 = await client.GetAsync("https://localhost:7051/api/Service/ServiceCount");
+            var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+            ViewBag.ServiceCount = jsonData1;
+
+
+
             return View();
         }
     }
