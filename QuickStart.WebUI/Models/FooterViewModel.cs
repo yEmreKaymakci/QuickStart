8a6f4a82a4ca8ed6dc2d0f87bdb35e2791f57397
yEmreKaymakci <yunusemrekymkci@gmail.com>
2026-03-11 12:19:19 +0300

    commit

---
+++ QuickStart.WebUI/Models/FooterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using QuickStart.WebUI.Dtos.Contacts;
+using QuickStart.WebUI.Dtos.Services;
+
+namespace QuickStart.WebUI.Models
+{
+    public class FooterViewModel
+    {
+        public ResultContactsDto Contact { get; set; }
+
+        public List<ResultServicesDto> Services { get; set; }
+    }
+}
